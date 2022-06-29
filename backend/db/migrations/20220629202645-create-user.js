0b53cd738ce19d82fe384bc2142e2e785779ce31
Francisco Palacios <Fpalacios926@gmail.com>
2022-06-29 13:40:36 -0700

    Add Users table migration and seeds

---
+++ backend/db/migrations/20220629202645-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNullL:false,
+        unique:true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull:false,
+        uniquet:true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};

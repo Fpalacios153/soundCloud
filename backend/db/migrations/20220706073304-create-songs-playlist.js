5062847d7dfd0e43192c1cc7a30d74b775f78911
Francisco Palacios <Fpalacios926@gmail.com>
2022-07-08 09:06:27 -0700

    added s back

--- backend/db/migrations/20220706073304-create-songs-playlist.js
+++ backend/db/migrations/20220706073304-create-songs-playlist.js
@@ -11,7 +11,7 @@ module.exports = {
       songId: {
         type: Sequelize.INTEGER,
         references: {
-          model:'Song',
+          model:'Songs',
           key:'id'
         },
         // onDelete: 'CASCADE'

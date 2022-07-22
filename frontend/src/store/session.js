1e95423a80abc4d837d782dbd04915130ea825f4
Francisco Palacios <Fpalacios926@gmail.com>
2022-07-22 00:58:21 -0700

    finished phase 2

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -29,6 +29,22 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');

3094a42e381e2da5cf9663794ec8a6a2f46c1e76
Francisco Palacios <Fpalacios926@gmail.com>
2022-08-28 23:49:59 -0700

    done with seeded data

--- backend/db/seeders/20220629203500-demo-user.js
+++ backend/db/seeders/20220629203500-demo-user.js
@@ -32,7 +32,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2', 'Bayside'] }
     }, {});
   }
 };

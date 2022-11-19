536ba2b44706579bbb40bae2577bc55f5e567e5c
Francisco Palacios <Fpalacios926@gmail.com>
2022-11-18 17:09:06 -0800

    set-up inital commments reducer

---
+++ frontend/src/store/comments.js
@@ -0,0 +1,41 @@
+import { csrfFetch } from "./csrf";
+
+
+//constants
+
+const GET_COMMENTS = 'comments/GET_COMMENTS'
+
+// action creator
+const getComments = commets => ({
+    type: GET_COMMENTS,
+    commets
+})
+// thunks
+
+export const getSongComments = (song) => async dispatch => {
+    const response = await csrfFetch(`/api/comments/${song.id}`)
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(getComments(data))
+        return data
+    }
+    return response
+}
+
+
+//reducer
+
+const initialState = {}
+const commentsReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case GET_COMMENTS:
+            const comments = {}
+            action.comments.forEach(com => {
+                comments[com.id] = com
+            });
+            return { ...comments }
+        default:
+            return state
+    }
+}

b45c0c53a5ddcb7214766e90b0c38d40b151066b
Francisco Palacios <Fpalacios926@gmail.com>
2023-01-10 17:16:33 -0800

    added basic stylings

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
         <>
             <div className="bottom-container-footer">
                 <div className="splash-about-container">
-                    <h2>About Help!</h2>
+                    <h2>About CloudSounds</h2>
                     <div className='splash-github-link'>
-                        <a className='github-links' href='https://github.com/Fpalacios153/Yelp-clone-capstone' rel="noreferrer" target='_blank'>CodeBase</a>
-                        <a className='github-links' href='https://github.com/Fpalacios153/Yelp-clone-capstone/blob/main/README.md' rel="noreferrer" target='_blank'>README</a>
+                        <a className='github-links' href="https://github.com/Fpalacios153/soundCloud" rel="noreferrer" target='_blank'>CodeBase</a>
+                        <a className='github-links' href='https://github.com/Fpalacios153/soundCloud/blob/main/README.md' rel="noreferrer" target='_blank'>README</a>
                     </div>
                 </div>
                 <div className="splash-discover-container">
@@ -17,7 +17,7 @@ export default function Footer() {
                         <a href='https://github.com/Fpalacios153' rel="noreferrer" target="_blank">
                             <i className="fa-brands fa-square-github devLinks fa-4x"></i>
                         </a>
-                        <a href="www.linkedin.com/in/francisco-palacios-783619253" rel="noreferrer" target="_blank">
+                        <a href="https://www.linkedin.com/in/francisco-palacios-783619253/" rel="noreferrer" target="_blank">
                             <i className="fa-brands fa-linkedin devLinks fa-4x"></i>
                         </a>
                     </div>
